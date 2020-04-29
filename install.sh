2130cc1b0d42c9e3fb25c2dfb9b1066235fba916
slo1 <26316200+slo1@users.noreply.github.com>
2020-04-29 17:31:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
 if [ ! -f ~/.tmux.conf ]; then
     ln -rs $PWD/tmux.conf ~/.tmux.conf
 fi
